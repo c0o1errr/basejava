cdc2eeecdf2b44a60b68420cbe7d3884444c109f
c0o1errr <d0theshka@gmail.com>
2020-03-23 21:44:55 +0300

    FirstHomeWork

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,55 @@
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
+
+public class ArrayStorage<delete> {
     Resume[] storage = new Resume[10000];
+    private String uuid;
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+
     }
 
     Resume get(String uuid) {
-        return null;
+                for (int i = 0; i < storage.length; i++) {
+                    if(storage[i].uuid == uuid) {
+                        return storage[i];
+                    }
+                }
+                return null;
+
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i].uuid = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newStorage = new Resume[10000];
+        for (int i = 0; i < storage.length; i++){
+            storage[i] = newStorage[i];
+        }
+        return newStorage;
     }
 
     int size() {
+        for (int i = 0; i < storage.length; i++){
+            // если считать, что массив отсортирован, но это не точно :'(
+            if (storage[i] == null) {
+                System.out.println("Длина массива = " + (i - 1));
+            }
+        }
         return 0;
     }
 }

4e0cee28b4d7c720c3586c5ce12ca3ccf7922bd5
c0o1errr <d0theshka@gmail.com>
2022-01-15 19:18:24 +0300

    add exception and throw Lesson04 Exception

--- src/com/basejava/webapp/exception/ExistStorageException.java
+++ src/com/basejava/webapp/exception/ExistStorageException.java
@@ -1,6 +1,6 @@
 package com.basejava.webapp.exception;
 
-public class gExistStorageException extends StorageException{
+public class ExistStorageException extends StorageException{
     public ExistStorageException( String uuid) {
         super( "Resume " + uuid + " already exist", uuid);
     }

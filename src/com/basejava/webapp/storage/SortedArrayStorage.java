76b939998cfbb0d91f251197504b5f1a4fdd23e9
c0o1errr <d0theshka@gmail.com>
2022-03-29 13:37:45 +0300

    Lesson06 Lambda

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -3,8 +3,20 @@ package com.basejava.webapp.storage;
 import com.basejava.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+    // Один из способов для обохода Comparable в Resume
+    /*
+    private static class ResumeComparator implements Comparator<Resume> {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    }
+    */
+    //lambda - выражение
+    private static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
 
     @Override
     protected void fillDeletedElement(int index) {
@@ -24,6 +36,6 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected Integer getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }

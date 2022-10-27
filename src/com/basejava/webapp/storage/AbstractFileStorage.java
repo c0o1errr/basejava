db038447d6d440638b5a380c37c38812e016d2fb
c0o1errr <d0theshka@gmail.com>
2022-10-27 23:29:15 +0700

    new AbstractAndObject

--- src/com/basejava/webapp/storage/AbstractFileStorage.java
+++ src/com/basejava/webapp/storage/AbstractFileStorage.java
@@ -3,14 +3,17 @@ package com.basejava.webapp.storage;
 import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public abstract class AbstractFileStorage extends AbstractStorage<File> {
     private File directory;
+
+    protected abstract void doWrite(Resume r, OutputStream file) throws IOException;
+
+    protected abstract Resume doRead(InputStream file) throws IOException;
 
     protected AbstractFileStorage(File directory) {
         Objects.requireNonNull(directory, "directory must not be null");
@@ -21,8 +24,40 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
             throw new IllegalArgumentException(directory.getAbsolutePath() + "is not readable/writable");
         }
         this.directory = directory;
+    }
 
+    @Override // получить все файлы и удалить методом delete
+    public void clear() {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                doDelete(file);
 
+            }
+        }
+    }
+
+    @Override // посчитать количество файлов
+    public int size() {
+        String[] list = directory.list();
+        if (list == null) {
+            throw new StorageException("Directory not be read", null);
+        }
+        return list.length;
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override // тоже самое что и в doSave только не создавать файл
+    protected void doUpdate(Resume r, File file) {
+        try {
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(file)));
+        } catch (IOException e) {
+            throw new StorageException("File write error", r.getUuid(), e);
+        }
     }
 
     @Override
@@ -30,49 +65,29 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
         return file.exists();
     }
 
-    @Override // тоже самое что и в doSave только не создавать файл
-    protected void doUpdate(Resume r, File file) {
-        try {
-            doWrite(r, file);
-        } catch (IOException e) {
-            throw new StorageException("File write error", r.getUuid(), e);
-        }
-    }
-
     @Override
     protected void doSave(Resume r, File file) {
         try {
             file.createNewFile();
-            doWrite(r, file);
         } catch (IOException e) {
             throw new StorageException("Couldn't create file" + file.getAbsolutePath(), file.getName(), e);
         }
         doUpdate(r, file);
-
-    }
-
-    protected abstract void doWrite(Resume r, File file) throws IOException;
-
-    protected abstract Resume doRead(File file) throws IOException;
-
-    /*@Override
-    protected File getSearchkey(String uuid) {
-        return new File(directory, uuid);
-    }*/
-
-    @Override
-    protected void doDelete(File file) {
-        if (!file.delete()) {
-            throw new StorageException("File delete error", file.getName());
-        }
     }
 
     @Override // создать абстрактый методо doRead который читает
     protected Resume doGet(File file) {
         try {
-            return doRead(file);
+            return doRead(new BufferedInputStream(new FileInputStream(file)));
         } catch (IOException e) {
             throw new StorageException("File read error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(File file) {
+        if (!file.delete()) {
+            throw new StorageException("File delete error", file.getName());
         }
     }
 
@@ -87,28 +102,5 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
             list.add(doGet(file));
         }
         return null;
-    }
-
-    @Override // получить все файлы и удалить методом delete
-    public void clear() {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                try {
-                    doRead(file);
-                } catch (IOException e) {
-                    throw new StorageException("File read error", file.getName(), e);
-                }
-            }
-        }
-    }
-
-    @Override // посчитать количество файлов
-    public int size() {
-        String[] list = directory.list();
-        if (list == null) {
-            throw new StorageException("Directory not be read", null);
-        }
-        return list.length;
     }
 }
--- src/com/basejava/webapp/storage/ObjectStreamStorage.java
+++ src/com/basejava/webapp/storage/ObjectStreamStorage.java
@@ -1,2 +1,22 @@
-package com.basejava.webapp.storage;public class ObjectStreamStorage {
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.io.*;
+
+public class ObjectStreamStorage extends AbstractFileStorage{
+
+    @Override
+    protected void doWrite(Resume r, OutputStream file) throws IOException {
+        
+    }
+
+    @Override
+    protected Resume doRead(InputStream file) throws IOException {
+        return null;
+    }
+
+    protected ObjectStreamStorage(File directory) {
+        super(directory);
+    }
 }

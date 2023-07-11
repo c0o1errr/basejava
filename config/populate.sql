5213a39ce6160bfd8174985afe2fe02277546950
c0o1errr <d0theshka@gmail.com>
2023-07-11 16:39:16 +0300

    исправил ошибки инициализации DB, все тесты прошли

---
+++ config/populate.sql
@@ -0,0 +1,4 @@
+INSERT INTO resume (uuid, full_name)
+VALUES ('4c8c4267-5a29-4fad-b05f-47bc9780bc3b', 'Name1'),
+       ('23655cf3-3bd1-4ec6-b5a7-869a2264277e', 'Name2'),
+       ('5080693f-33ca-4260-910d-9fba41e3dd24', 'Name3')
